542a7c8129f993841e6905ca0a6859deeb1faf40
Frozha <kondalkarhrithvik@gmail.com>
2023-05-15 22:46:17 +0530

    file creation

---
+++ assignment1/prime.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    printf("Enter range to find prime numbers \n from - ");
+    int lower;
+    scanf("%d",&lower);
+    printf("to - ");
+    int upper;
+    scanf("%d",&upper);
+    int composite_bool;
+    for(int i=lower;i<=upper;i++){
+        composite_bool =0;
+        for(int k = 2;k<i/1.414;k++){
+            if (i%k==0){
+                composite_bool=1;
+                break;
+            }
+        }
+        if(composite_bool ==0){
+            printf("%d ",i);
+        }
+    }
+}
